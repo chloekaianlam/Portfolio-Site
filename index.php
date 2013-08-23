3930c72308ddb808537f8f8446eadfb12920b812
Chloe Kaian Lam <chloe.kaian.lam@me.com>
2013-08-23 12:56:42 +0100

    Vectors and backup

--- index.php
+++ index.php
@@ -7,9 +7,11 @@
 		<div class="content">
 			<h1>Home</h1>
 
-			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#" data-hover="Saatchi &amp; Saatchi">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
-			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at the <a href="#" data-hover="Love">Love</a> section of my little world.</p>
+			<p>There are many titles to describe what I do, ultimately I design and code details for the internet. Currently I am a Front End Developer at <a href="#" data-hover="Saatchi &amp; Saatchi">Saatchi &amp; Saatchi</a> by day, a Creative Freelancer by night and weekends. Yes, that's right! I dedicate most of my time designing and coding stuff, because I LOVE it.</p>
+			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at <a href="#" data-hover="Love">Love</a> of my little world.</p>
 		</div>
+
+		<figure></figure>
 
 	</article>
 
@@ -21,6 +23,8 @@
 			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
 			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at the <a href="#">Love</a> section of my little world.</p>
 		</div>
+
+		<figure></figure>
 		
 	</article>
 
@@ -32,6 +36,8 @@
 			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
 			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at the <a href="#">Love</a> section of my little world.</p>
 		</div>
+
+		<figure></figure>
 		
 	</article>
 
@@ -43,6 +49,8 @@
 			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
 			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at the <a href="#">Love</a> section of my little world.</p>
 		</div>
+
+		<figure></figure>
 		
 	</article>
 
