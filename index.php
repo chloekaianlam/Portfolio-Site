7b73d11ded521eafb9fcb0be9945064b8a73d319
Chloe Kaian Lam <chloe.kaian.lam@me.com>
2013-08-29 15:13:58 +0100

    Created clients and contact section
    Added form and logos

--- index.php
+++ index.php
@@ -19,11 +19,20 @@
 
 	<article class="projects" data-page="projects" data-colour="yellow">
 		
-		<div class="content">
+		<div class="wrapper">
 			<h1>Projects</h1>
 
-			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
-			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at the <a href="#">Love</a> section of my little world.</p>
+			<div class="project-grid">
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+				<img src="http://lorempixel.com/300/200/animals/" />
+			</div>
 		</div>
 
 		<figure></figure>
@@ -32,7 +41,7 @@
 
 	<article class="love" data-page="love" data-colour="orange">
 		
-		<div class="content">
+		<div class="wrapper">
 			<h1>Love</h1>
 
 			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
@@ -45,11 +54,23 @@
 
 	<article class="contact" data-page="contact" data-colour="lime">
 		
-		<div class="content">
+		<div class="wrapper">
 			<h1>Contact</h1>
 
-			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
-			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at the <a href="#">Love</a> section of my little world.</p>
+			<form>
+
+				<label for="name">Name:</label><img class="icon" src="assets/image/icon/pencil.png" />
+				<input type="text" name="name" placeholder="Name" />
+
+				<label for="email">Email:</label><img class="icon" src="assets/image/icon/email.png" />
+				<input type="text" name="email" placeholder="Email" />
+
+				<label for="message">Message:</label><img class="icon" src="assets/image/icon/message.png" />
+				<textarea rows="8"></textarea>
+
+				<input type="submit" value="Send" class="button" />
+
+			</form>
 		</div>
 
 		<figure></figure>
@@ -58,11 +79,26 @@
 
 	<article class="client" data-page="client" data-colour="purple">
 		
-		<div class="content">
+		<div class="wrapper">
 			<h1>Clients</h1>
 
-			<p>There are plently of titles to describe what I do, currently I am a Front End Developer at <a href="#">Saatchi &amp; Saatchi</a> by day, also a Creative Freelancer by night and weekends. Yes, that's right! I dedicate pretty much all of my time designing and coding stuff, because I LOVE it.</p>
-			<p>In case you haven't noticed I like showcasing the countries I lived and worked in, I am currently living in London, loving every second of it. Obviously, there are other things I love and feel passionate about, if you are interested take a look at the <a href="#">Love</a> section of my little world.</p>
+			<div class="client-grid">
+				<img src="assets/image/logos/rekorderlig.png" />
+				<img src="assets/image/logos/visa.png" />
+				<img src="assets/image/logos/coca-cola.png" />
+				<img src="assets/image/logos/tnt.png" />
+				<img src="assets/image/logos/nhs.png" />
+				<img src="assets/image/logos/hello-kitty.png" />
+				<img src="assets/image/logos/candyland.png" />
+				<img src="assets/image/logos/shaun-the-sheep.png" />
+				<img src="assets/image/logos/disney.png" />
+				<img src="assets/image/logos/macau-grand-prix.png" />
+				<img src="assets/image/logos/ibm.png" />
+				<img src="assets/image/logos/fujitsu.png" />
+				<img src="assets/image/logos/siemens.png" />
+				<img src="assets/image/logos/hp.png" />
+				<img src="assets/image/logos/sainsburys.png" />
+			</div>
 		</div>
 
 		<figure></figure>
